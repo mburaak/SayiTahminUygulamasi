01943e91d07b1a2d71e7aeff09e5560ff79fcfcd
Muhsin Burak Demirtaş <muhsin.demirtas2@gmail.com>
2025-12-30 17:11:47 +0300

    g

--- App.js
+++ App.js
@@ -4,18 +4,29 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/color';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
-
+  const [gameIsOver, setGameIsOver] = useState(true);
   function pickedNumberHandler(pickedNumber){
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+    function gameOverHandler(){
+    setGameIsOver(true);
   }
 
   let screen = <StartGameScreen onPickNumber = {pickedNumberHandler}/>;
   if(userNumber){
-    screen = <GameScreen />;
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
   }
+
+  if(gameIsOver && userNumber){
+    screen = <GameOverScreen />;
+  }
+
 
   return (
     <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.container}>
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,7 @@
-function GameOverScreen(){
+import { Text } from "react-native";
 
+function GameOverScreen(){
+    return <Text>Game Over!</Text>;
 }
 
 export default GameOverScreen;
